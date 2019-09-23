93f775b794bfe50ea7a40d9f14625947eff409ee
Jeffery Eckert <bromodo@gmail.com>
2019-09-23 02:06:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,61 @@
 def map_to_negativize(array)
   final_array = []
   counter = 0 
-   while counter < array.size 
-   final_array.push{array[counter] * -1}
+   while counter < array.size do
+   final_array.push(array[counter] * -1)
    counter += 1
    end
    return final_array
  end
 
 
+def map_to_no_change(array)
+  final_array = []
+  counter = 0 
+   while counter < array.size do
+   final_array.push(array[counter])
+   counter += 1
+   end
+   return final_array
+ end
+ 
+ 
+ def map_to_double(array)
+  final_array = []
+  counter = 0 
+   while counter < array.size do
+   final_array.push(array[counter] * 2)
+   counter += 1
+   end
+   return final_array
+ end
+
+def map_to_square(array)
+  final_array = []
+  counter = 0 
+   while counter < array.size do
+   final_array.push(array[counter] ** 2)
+   counter += 1
+   end
+   return final_array
+ end
+ 
+ 
+ def reduce_to_total(array, starting_point=0)
+   total = starting_point
+  counter = 0 
+   while counter < array.size do
+  total += array[counter]
+   counter += 1
+   end
+   return total
+ end
+ 
+ def reduce_to_total(array)
+  counter = 0 
+   while counter < array.size do
+  return false if array[counter].include? Integer
+   counter += 1
+   end
+   return true
+ end
